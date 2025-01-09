e5aaa4a528e3d9252208a2f2481a3ae125486bf7
zBlade <xshadowblade618@gmail.com>
2025-01-08 20:15:39 -0500

    feat: add old code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,28 +1,20 @@
-// Copyright 2021-2025 FRC 6328
-// http://github.com/Mechanical-Advantage
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// version 3 as published by the Free Software Foundation or
-// available in the root directory of this project.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.lib.util.loggingUtil.LogManager;
+// Advantage kit utils
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
-import org.littletonrobotics.urcl.URCL;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -31,10 +23,22 @@ import org.littletonrobotics.urcl.URCL;
  * project.
  */
 public class Robot extends LoggedRobot {
-    private Command autonomousCommand;
-    private RobotContainer robotContainer;
 
-    public Robot() {
+    private Command m_autonomousCommand;
+
+    private RobotContainer m_robotContainer;
+
+    private static final String defaultAuto = "Default";
+    private static final String customAuto = "My Auto";
+    private String autoSelected;
+    private final LoggedDashboardChooser<String> chooser = new LoggedDashboardChooser<>("Auto Choices");
+
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
         // Record metadata
         Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
         Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
@@ -60,12 +64,10 @@ public class Robot extends LoggedRobot {
                 Logger.addDataReceiver(new WPILOGWriter());
                 Logger.addDataReceiver(new NT4Publisher());
                 break;
-
             case SIM:
                 // Running a physics simulator, log to NT
                 Logger.addDataReceiver(new NT4Publisher());
                 break;
-
             case REPLAY:
                 // Replaying a log, set up replay source
                 setUseTiming(false); // Run as fast as possible
@@ -75,50 +77,53 @@ public class Robot extends LoggedRobot {
                 break;
         }
 
-        // Initialize URCL
-        Logger.registerURCL(URCL.startExternal());
+        // See http://bit.ly/3YIzFZ6 for more information on timestamps in AdvantageKit.
+        // Logger.disableDeterministicTimestamps()
 
         // Start AdvantageKit logger
         Logger.start();
 
-        // Instantiate our RobotContainer. This will perform all our button bindings,
-        // and put our autonomous chooser on the dashboard.
-        robotContainer = new RobotContainer();
+        // Initialize auto chooser
+        chooser.addDefaultOption("Default Auto", defaultAuto);
+        chooser.addOption("My Auto", customAuto);
+
+        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        // autonomous chooser on the dashboard.
+        m_robotContainer = new RobotContainer();
     }
 
-    /** This function is called periodically during all modes. */
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for items like
+     * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
     @Override
     public void robotPeriodic() {
-        // Switch thread to high priority to improve loop timing
-        Threads.setCurrentThreadPriority(true, 99);
-
-        // Runs the Scheduler. This is responsible for polling buttons, adding
-        // newly-scheduled commands, running already-scheduled commands, removing
-        // finished or interrupted commands, and running subsystem periodic() methods.
-        // This must be called from the robot's periodic block in order for anything in
-        // the Command-based framework to work.
+        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+        // commands, running already-scheduled commands, removing finished or interrupted commands,
+        // and running subsystem periodic() methods.  This must be called from the robot's periodic
+        // block in order for anything in the Command-based framework to work.
         CommandScheduler.getInstance().run();
-
-        // Return to normal thread priority
-        Threads.setCurrentThreadPriority(false, 10);
+        LogManager.log();
     }
 
-    /** This function is called once when the robot is disabled. */
+    /** This function is called once each time the robot enters Disabled mode. */
     @Override
     public void disabledInit() {}
 
-    /** This function is called periodically when disabled. */
     @Override
     public void disabledPeriodic() {}
 
     /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
     @Override
     public void autonomousInit() {
-        autonomousCommand = robotContainer.getAutonomousCommand();
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
         // schedule the autonomous command (example)
-        if (autonomousCommand != null) {
-            autonomousCommand.schedule();
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.schedule();
         }
     }
 
@@ -126,15 +131,14 @@ public class Robot extends LoggedRobot {
     @Override
     public void autonomousPeriodic() {}
 
-    /** This function is called once when teleop is enabled. */
     @Override
     public void teleopInit() {
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        if (autonomousCommand != null) {
-            autonomousCommand.cancel();
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
         }
     }
 
@@ -142,7 +146,6 @@ public class Robot extends LoggedRobot {
     @Override
     public void teleopPeriodic() {}
 
-    /** This function is called once when test mode is enabled. */
     @Override
     public void testInit() {
         // Cancels all running commands at the start of test mode.
@@ -152,12 +155,4 @@ public class Robot extends LoggedRobot {
     /** This function is called periodically during test mode. */
     @Override
     public void testPeriodic() {}
-
-    /** This function is called once when the robot is first started up. */
-    @Override
-    public void simulationInit() {}
-
-    /** This function is called periodically whilst in simulation. */
-    @Override
-    public void simulationPeriodic() {}
 }
---
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.swerve;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+/** A swerve module. Used to control a single swerve module on the robot. */
+public interface SwerveModule {
+    public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop);
+
+    public Rotation2d getCanCoder();
+
+    public SwerveModuleState getState();
+
+    public SwerveModulePosition getPosition();
+
+    public int getModuleNumber();
+
+    public void setModuleNumber(int moduleNumber);
+}
