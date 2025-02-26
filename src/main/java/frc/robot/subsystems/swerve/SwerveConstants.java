a0250a39527023b67f9688412d6b7554d1780892
GraysonFRC <91495348+GraysonFRC@users.noreply.github.com>
2025-02-26 14:54:34 -0500

    chore: update CAN ids

--- src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveConstants.java
@@ -20,9 +20,9 @@ public final class SwerveConstants {
         public static final class Mod0 {
             private Mod0() {}
 
-            public static final int DRIVE_MOTOR_ID = 4;
+            public static final int DRIVE_MOTOR_ID = 12;
             public static final int ANGLE_MOTOR_ID = 3;
-            public static final int CANCODER_ID = 13;
+            public static final int CANCODER_ID = 14;
             public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees(17.402344);
             public static final RevSwerveModuleConstants constants =
                     new RevSwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CANCODER_ID, ANGLE_OFFSET);
@@ -32,9 +32,9 @@ public final class SwerveConstants {
         public static final class Mod1 {
             private Mod1() {}
 
-            public static final int DRIVE_MOTOR_ID = 6;
-            public static final int ANGLE_MOTOR_ID = 5;
-            public static final int CANCODER_ID = 14;
+            public static final int DRIVE_MOTOR_ID = 5;
+            public static final int ANGLE_MOTOR_ID = 2;
+            public static final int CANCODER_ID = 13;
             public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees((79.277344) + 180.0);
             public static final RevSwerveModuleConstants constants =
                     new RevSwerveModuleConstants(DRIVE_MOTOR_ID, ANGLE_MOTOR_ID, CANCODER_ID, ANGLE_OFFSET);
@@ -44,8 +44,8 @@ public final class SwerveConstants {
         public static final class Mod2 {
             private Mod2() {}
 
-            public static final int DRIVE_MOTOR_ID = 2;
-            public static final int ANGLE_MOTOR_ID = 1;
+            public static final int DRIVE_MOTOR_ID = 4;
+            public static final int ANGLE_MOTOR_ID = 10;
             public static final int CANCODER_ID = 15;
             public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees((73.740234));
             public static final RevSwerveModuleConstants constants =
@@ -56,8 +56,8 @@ public final class SwerveConstants {
         public static final class Mod3 {
             private Mod3() {}
 
-            public static final int DRIVE_MOTOR_ID = 8;
-            public static final int ANGLE_MOTOR_ID = 9;
+            public static final int DRIVE_MOTOR_ID = 1;
+            public static final int ANGLE_MOTOR_ID = 8;
             public static final int CANCODER_ID = 16;
             public static final Rotation2d ANGLE_OFFSET = Rotation2d.fromDegrees((185.273438) + 180.0);
             public static final RevSwerveModuleConstants constants =
