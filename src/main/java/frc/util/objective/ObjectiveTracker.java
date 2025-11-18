eb416c6ccd759380495fbd9dd16f384def89c209
zBlade <xshadowblade618@gmail.com>
2025-11-18 14:10:52 -0500

    wip(objective): add objective tracker

---
+++ src/main/java/frc/util/objective/ObjectiveTracker.java
@@ -0,0 +1,9 @@
+package frc.util.objective;
+
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import org.littletonrobotics.junction.networktables.LoggedNetworkString;
+
+public class ObjectiveTracker {
+    // public enum
+}
